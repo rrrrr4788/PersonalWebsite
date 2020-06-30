eaa16df84569ada86eb0bf574467c6d080abd893
Haozhe Zhang <zhz19980514@ufl.edu>
2020-06-30 13:41:09 -0400

    Updates

---
+++ precache-manifest.b5de90f2483f5b6820c3ca15fae17b70.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1e11f74155fa8705487b689bd121bff",
+    "url": "/PersonalWebsite/index.html"
+  },
+  {
+    "revision": "93711e219effde13e16a",
+    "url": "/PersonalWebsite/static/css/main.d53c7b36.chunk.css"
+  },
+  {
+    "revision": "9d1000b5addeb1abf803",
+    "url": "/PersonalWebsite/static/js/2.67edec80.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PersonalWebsite/static/js/2.67edec80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93711e219effde13e16a",
+    "url": "/PersonalWebsite/static/js/main.858b1d23.chunk.js"
+  },
+  {
+    "revision": "5e2ede022b3d6dfb6364",
+    "url": "/PersonalWebsite/static/js/runtime-main.2a09b87b.js"
+  },
+  {
+    "revision": "de2cf3ea9b841229f08851463b340d2e",
+    "url": "/PersonalWebsite/static/media/C++.de2cf3ea.png"
+  },
+  {
+    "revision": "b54fc1c9e4f229859a1b00205f5d14c0",
+    "url": "/PersonalWebsite/static/media/JS.b54fc1c9.png"
+  },
+  {
+    "revision": "2c333c67eea4fb05574bce212f70a727",
+    "url": "/PersonalWebsite/static/media/Java.2c333c67.png"
+  },
+  {
+    "revision": "5ae8f58ef59f2c4d08f16202cf8c39df",
+    "url": "/PersonalWebsite/static/media/Python.5ae8f58e.png"
+  },
+  {
+    "revision": "c7f92fc8ed96d918aadee1b51c717ac4",
+    "url": "/PersonalWebsite/static/media/android.c7f92fc8.png"
+  },
+  {
+    "revision": "10550ee86b47ea33b21c4df8c6a053e8",
+    "url": "/PersonalWebsite/static/media/css.10550ee8.png"
+  },
+  {
+    "revision": "842b36f1e7664a8cd3c10d286402dfa2",
+    "url": "/PersonalWebsite/static/media/experiences.842b36f1.jpg"
+  },
+  {
+    "revision": "884870919590040200b5d3f014368a4b",
+    "url": "/PersonalWebsite/static/media/express.88487091.png"
+  },
+  {
+    "revision": "dae7f44af6bf5c3f4b6eeb4d65eb2bde",
+    "url": "/PersonalWebsite/static/media/firebase.dae7f44a.png"
+  },
+  {
+    "revision": "bf9aef7541b42f87f48a2b9afbb9a976",
+    "url": "/PersonalWebsite/static/media/headshot.bf9aef75.png"
+  },
+  {
+    "revision": "fb1e7f9db2540c3194a9179094a925e2",
+    "url": "/PersonalWebsite/static/media/html.fb1e7f9d.png"
+  },
+  {
+    "revision": "ae6ce5d4dd12a7b195d22c1d54ce4ef4",
+    "url": "/PersonalWebsite/static/media/kivy.ae6ce5d4.png"
+  },
+  {
+    "revision": "0fab12847e7771ade81144c5feda884a",
+    "url": "/PersonalWebsite/static/media/matlab.0fab1284.png"
+  },
+  {
+    "revision": "67db45dce58818968867e8230254528c",
+    "url": "/PersonalWebsite/static/media/node.67db45dc.png"
+  },
+  {
+    "revision": "19a4fc58e4742a466e723d43a051b760",
+    "url": "/PersonalWebsite/static/media/person-1.19a4fc58.jpg"
+  },
+  {
+    "revision": "9090cde8a2f7d7d2bb086f0c262c5494",
+    "url": "/PersonalWebsite/static/media/person-2.9090cde8.jpg"
+  },
+  {
+    "revision": "39d1d423f738afd1a2bedd95e510d414",
+    "url": "/PersonalWebsite/static/media/profile.39d1d423.png"
+  },
+  {
+    "revision": "cf9d26c3c13653602c28a7dd0a75d123",
+    "url": "/PersonalWebsite/static/media/react.cf9d26c3.png"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/PersonalWebsite/static/media/showcase.4b31330b.jpg"
+  },
+  {
+    "revision": "f2e09e69e1e929002af8b7f13ff81cf6",
+    "url": "/PersonalWebsite/static/media/skillset-2.f2e09e69.jpg"
+  },
+  {
+    "revision": "9a0989e719aad8353e6f594c3a173600",
+    "url": "/PersonalWebsite/static/media/skillset-3.9a0989e7.jpeg"
+  },
+  {
+    "revision": "437ebf4d96701f9c146bf24123f77753",
+    "url": "/PersonalWebsite/static/media/unity.437ebf4d.png"
+  },
+  {
+    "revision": "8911b2a812489868a3e651a228c23bd1",
+    "url": "/PersonalWebsite/static/media/web.8911b2a8.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rrrrr4788/rrrrr4788.github.io/precache-manifest.670d352022fe26d179c1a8a8468239e9.js"
+  "/PersonalWebsite/precache-manifest.b5de90f2483f5b6820c3ca15fae17b70.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rrrrr4788/rrrrr4788.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PersonalWebsite/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
